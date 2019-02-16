095513303fe97d6d1cd32062a84544f7099875ba
TerenceJing <Terence_Jing@163.com>
2019-02-16 10:18:19 +0800

    初始化工程

---
+++ training-web/src/main/java/com/terence/itech/training/web/TrainingWebApplication.java
@@ -0,0 +1,14 @@
+package com.terence.itech.training.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingWebApplication.class, args);
+	}
+
+}
+
