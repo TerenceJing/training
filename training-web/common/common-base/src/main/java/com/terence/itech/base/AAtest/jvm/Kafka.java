0560156a2ae262936fe554f8b210cfb41f8211c4
Terence_Jing@163.com <Terence_Jing@163.com>
2021-01-22 17:32:47 +0800

    [add]添加可被回收的对象记录

--- training-web/common/common-base/src/main/java/com/terence/itech/base/AAtest/jvm/Kafka.java
+++ training-web/common/common-base/src/main/java/com/terence/itech/base/AAtest/jvm/Kafka.java
@@ -1,13 +1,24 @@
 package com.terence.itech.base.AAtest.jvm;
 
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+
 /**
  * @author tiancai
  * @version $$Id: Kafka, v 0.1 2020/7/8 11:28 ***  Exp $$
  */
 public class Kafka {
+  private static Kafka instance;
   public  static  void main(String[] args) {
     Manager manager=new Manager();
     Boolean local=manager.isLocalData();
     System.out.println(local);
+  }
+
+  @Override
+  public void finalize(){
+    Kafka.instance=this;
+    SoftReference<Kafka> soft=new SoftReference<>(new Kafka());
+    WeakReference<Kafka> weak=new WeakReference<>(new Kafka());
   }
 }
